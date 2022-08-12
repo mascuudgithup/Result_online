67bf2b8de6527cf3b9eb8f7212b95277c38f5883
Mesud abdirahman <108215018+mascuudgithup@users.noreply.github.com>
2022-08-12 12:15:30 +0300

    first commit

--- config/header.php
+++ config/header.php
@@ -45,9 +45,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>O</b>NL</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>INE</b>RESULT</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -367,7 +367,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="student.php"><i class="fa fa-circle-o"></i> Invoice</a></li>
+            <li><a href="order.php"><i class="fa fa-circle-o"></i> Invoice</a></li>
       
           </ul>
         </li>
---
+++ order.php
@@ -0,0 +1,70 @@
+<?php
+include "config/header.php";
+
+?>
+
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+      Create Order
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i>level</a></li>
+        <li><a href="#">hare</a></li>
+        
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          
+
+          <div class="box box-warning">
+            <form  action="" method="post" name="">
+
+            <h3 class="box-title">New Order</h3>
+
+<!-- this is box -form -->
+
+            <div class="box-body">
+<div class="col-md-6">
+<div class="form-group">
+  <label for="#">CostomerName</label>
+  <input type="text" class="form-control" name="txtcustomer" placeholder="enter costomer name">
+</div>
+
+</div>
+
+<div class="col-md-6">
+  <h2>date</h2>
+</div>
+
+
+            </div><!-- this is box -form -->
+            <div class="box-body"></div><!-- this is box -table-->
+            <div class="box-body"></div><!-- this is box -total -->
+            </form>
+
+     
+          </div>
+        
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+<?php
+include("config/footer.php");
+
+?>
+
